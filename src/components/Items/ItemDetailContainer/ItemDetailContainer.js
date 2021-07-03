390e429c8403d5494dc4619af4a25d007ac93ad1
Yohanna Etchemendy <yohanna.j.e@gmail.com>
2021-07-03 17:09:26 -0300

    Ejercicio Clase 12
    +Firebase
    - Async mocks promises

--- src/components/Items/ItemDetailContainer/ItemDetailContainer.js
+++ src/components/Items/ItemDetailContainer/ItemDetailContainer.js
@@ -4,6 +4,7 @@ import "./ItemDetailContainer.css";
 import { useParams } from "react-router-dom";
 import { Loader } from "../../Loader.js";
 import {dataBase} from "../../../Firebase/firebase.js"
+import { SettingsRemoteSharp } from "@material-ui/icons";
 
 export const ItemDetailContainer = () => {
   //utilizao useState para registrar y mostrar los cambios en pantalla.
@@ -12,101 +13,21 @@ export const ItemDetailContainer = () => {
   const { id } = useParams();
   //Creo una funcion para obtener mis datos, los cuales se resuelven en una
   //promise con timeOut para emular el backend.
-  const getAllProducts = () => {
-    return new Promise((resolve, reject) => {
-      setTimeout(
-        () =>
-          resolve(
-            [
-            {id:'1', 
-            img: 'https://images.unsplash.com/photo-1594908629102-237d9c0f9594?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fGNhY3R1c3xlbnwwfDJ8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60',
-            name: 'Senecio peregrinus', 
-            stock: 10, 
-            min:1,
-            category:'cactus',
-            description: 'Se trata de plantas muy resistentes con capacidad para almacenar agua en sus hojas.', 
-            price:250},
-            {
-            id:'2', 
-            img: 'https://images.unsplash.com/photo-1621778764188-bf87fbb77c7b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Njd8fGNhY3R1c3xlbnwwfDJ8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60',
-            name: 'Rhipsalis cereuscula', 
-            stock: 5, 
-            min:1,
-            category:'interior',
-            description: 'Se trata de un cactus carente de espinas que en su hábitat natural prospera sobre árboles.', 
-            price:250},
-            {
-            id:'3', 
-            img: 'https://images.unsplash.com/photo-1564335375227-9983c3898775?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjN8fGNhY3R1c3xlbnwwfDJ8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60',
-            name: 'Kalanchoe', 
-            stock: 6, 
-            min:1,
-            category:'exterior',
-            description: 'Puede alcanzar los 6 m de altura, sin embargo, la mayoría no superan 1 m.', 
-            price:250},
-            {id:'4', 
-            img: 'https://images.unsplash.com/photo-1596732402501-9e5d6d62b13e?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzV8fGNhY3R1c3xlbnwwfDJ8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60',
-            name: 'Kalanchoe', 
-            stock: 4, 
-            min:1,
-            category:'interior',
-            description: 'Puede alcanzar los 6 m de altura, sin embargo, la mayoría no superan 1 m.', 
-            price:250},
-            {
-            id:'5', 
-            img: 'https://images.unsplash.com/photo-1594908629102-237d9c0f9594?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fGNhY3R1c3xlbnwwfDJ8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60',
-            name: 'Senecio peregrinus', 
-            stock: 9, 
-            min:1,
-            category:'cactus',
-            description: 'Se trata de plantas muy resistentes con capacidad para almacenar agua en sus hojas.', 
-            price:250},
-            {
-            id:'6', 
-            img: 'https://images.unsplash.com/photo-1621778764188-bf87fbb77c7b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Njd8fGNhY3R1c3xlbnwwfDJ8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60',
-            name: 'Rhipsalis cereuscula', 
-            stock: 20, 
-            min:1,
-            category:'exterior',
-            description: 'Se trata de un cactus carente de espinas que en su hábitat natural prospera sobre árboles.', 
-            price:250},
-            {
-            id:'7', 
-            img: 'https://images.unsplash.com/photo-1564335375227-9983c3898775?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjN8fGNhY3R1c3xlbnwwfDJ8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60',
-            name: 'Kalanchoe', 
-            stock: 2, 
-            min:1,
-            category:'interior',
-            description: 'Puede alcanzar los 6 m de altura, sin embargo, la mayoría no superan 1 m.', 
-            price:250},
-            {
-            id:'8', 
-            img: 'https://images.unsplash.com/photo-1596732402501-9e5d6d62b13e?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzV8fGNhY3R1c3xlbnwwfDJ8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60',
-            name: 'Kalanchoe', 
-            stock: 8, 
-            min:1,
-            category:'cactus',
-            description: 'Puede alcanzar los 6 m de altura, sin embargo, la mayoría no superan 1 m.', 
-            price:250}
-            ]
-          ),
-        2000
-      );
-    });
-  };
+  
 
-  //utilizo UseEffect para simplificar lo que tengo que hacer
-  //primero llamo a la funcion que tiene la promise y utilizo .then para el caso de exito
-  //luego al resultado de la promise (mi array de objetos), lo paso
-  //como parametro a mi funcion de estado setProdById pero ya filtrados por
-  //el valor de Id que tome de useParams, de esta manera ya mando directamente
-  //el item que necesito mostrar, y es el unico que se guarda en mi constante
-  //de estado.
 
   useEffect(() => {
-    getAllProducts().then((data) => {
-      setProdById(data.find(prod => prod.id === id));
-    });
+    const itemCollection = dataBase.collection("items");
+    const item = itemCollection.doc(id);
+
+    item.get().then((doc)=>{
+      if(!doc.exists){
+        console.log('item doesnt exist');
+      }
+      setProdById({id: doc.id, ...doc.data()})
+    })
+    
+   
   }, []);
 
   //Para poder mostrar el loader y evaluar si mi objeto estaba inicializado (vacio)
--- src/components/Items/ItemListContainer/ItemListContainer.js
+++ src/components/Items/ItemListContainer/ItemListContainer.js
@@ -12,110 +12,21 @@ export const ItemListContainer = () => {
   //lo inicializo en [] ya que va a contener un array con
   //mis objetos -> items a vender
   const [items, setItems] = useState([]);
-  //Creo una funcion getItems que crea una promise donde  
-  //se obtienen mis datos, y la resuelvo con un
-  //setTimeOut para emular el pedido al backend
-
-  const getItems = () => {
-    return new Promise((resolve, reject) => {
-      setTimeout(
-        () =>
-          resolve([
-            {
-              id: "1",
-              img: "https://images.unsplash.com/photo-1594908629102-237d9c0f9594?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fGNhY3R1c3xlbnwwfDJ8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60",
-              name: "Senecio peregrinus",
-              stock: true,
-              category: "cactus",
-              description:
-                "Se trata de plantas muy resistentes con capacidad para almacenar agua en sus hojas.",
-              price: 250,
-            },
-            {
-              id: "2",
-              img: "https://images.unsplash.com/photo-1621778764188-bf87fbb77c7b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Njd8fGNhY3R1c3xlbnwwfDJ8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60",
-              name: "Rhipsalis cereuscula",
-              stock: true,
-              category: "interior",
-              description:
-                "Se trata de un cactus carente de espinas que en su hábitat natural prospera sobre árboles.",
-              price: 200,
-            },
-            {
-              id: "3",
-              img: "https://images.unsplash.com/photo-1564335375227-9983c3898775?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjN8fGNhY3R1c3xlbnwwfDJ8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60",
-              name: "Kalanchoe",
-              stock: true,
-              category: "exterior",
-              description:
-                "Puede alcanzar los 6 m de altura, sin embargo, la mayoría no superan 1 m.",
-              price: 320,
-            },
-            {
-              id: "4",
-              img: "https://images.unsplash.com/photo-1596732402501-9e5d6d62b13e?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzV8fGNhY3R1c3xlbnwwfDJ8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60",
-              name: "Kalanchoe",
-              stock: true,
-              category: "interior",
-              description:
-                "Puede alcanzar los 6 m de altura, sin embargo, la mayoría no superan 1 m.",
-              price: 180,
-            },
-            {
-              id: "5",
-              img: "https://images.unsplash.com/photo-1594908629102-237d9c0f9594?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fGNhY3R1c3xlbnwwfDJ8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60",
-              name: "Senecio peregrinus",
-              stock: true,
-              category: "cactus",
-              description:
-                "Se trata de plantas muy resistentes con capacidad para almacenar agua en sus hojas.",
-              price: 350,
-            },
-            {
-              id: "6",
-              img: "https://images.unsplash.com/photo-1621778764188-bf87fbb77c7b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Njd8fGNhY3R1c3xlbnwwfDJ8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60",
-              name: "Rhipsalis cereuscula",
-              stock: true,
-              category: "exterior",
-              description:
-                "Se trata de un cactus carente de espinas que en su hábitat natural prospera sobre árboles.",
-              price: 250,
-            },
-            {
-              id: "7",
-              img: "https://images.unsplash.com/photo-1564335375227-9983c3898775?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjN8fGNhY3R1c3xlbnwwfDJ8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60",
-              name: "Kalanchoe",
-              stock: true,
-              category: "interior",
-              description:
-                "Puede alcanzar los 6 m de altura, sin embargo, la mayoría no superan 1 m.",
-              price: 260,
-            },
-            {
-              id: "8",
-              img: "https://images.unsplash.com/photo-1596732402501-9e5d6d62b13e?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzV8fGNhY3R1c3xlbnwwfDJ8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60",
-              name: "Kalanchoe",
-              stock: true,
-              category: "cactus",
-              description:
-                "Puede alcanzar los 6 m de altura, sin embargo, la mayoría no superan 1 m.",
-              price: 375,
-            },
-          ]),
-        2000
-      );
-    });
-  };
-
- //uso useEffect para guardar los datos en mi variable de estado
- //llamo a getItems y utilizo .then porque me interesa el caso en que 
- //la promise se resolvio correctamente.
-
+ 
   useEffect(() => {
-    getItems().then((data) => {
-      setItems(data);
-    });
-  }, [id]); //aca utilizo ID para que se dispare useEffect cada vez que
+    const itemCollection = dataBase.collection("items");
+    itemCollection.get().then((response)=>{
+      if(response.size===0){
+        console.log("no results");
+      }
+      const ItemsDB = response.docs.map(element=>{
+        return {...element.data(), id:element.id};
+      });
+      setItems(ItemsDB);
+    })
+  }, [id]); 
+  
+  //aca utilizo ID para que se dispare useEffect cada vez que
   //mi valor id (obtenido por useparams) cambie, o sea cada vez que 
   //tenga que cargar otra categoria.
 
