06c60a71781f53718d3cf3135facbd97ad143cd2
Yohanna Etchemendy <yohanna.j.e@gmail.com>
2021-07-23 00:47:07 -0300

    files cleanup

--- src/components/NavBar/TagWidget/TagWidget.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import tagIcon from './tag.svg';
-
-export const TagWidget = () => {
-
-    return <div className="tagWidget">
-        <img src={tagIcon} alt="Precio"></img>
-    </div>
-}
